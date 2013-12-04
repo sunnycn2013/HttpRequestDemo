542676dec94c8e333d9afab3b61dff5de62082c1
guo <389878212@qq.com>
2013-12-04 17:36:26 +0800

    Initial Commit

---
+++ HttpRequestDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HttpRequestDemo
+//
+//  Created by Qeebu on 13-12-4.
+//  Copyright (c) 2013年 Qeebu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
